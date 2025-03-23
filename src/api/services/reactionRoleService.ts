58448a45621772f8df778b9c9a0fbbf92ac7d408
silentesc <scope-frisk-copy@duck.com>
2025-03-23 12:48:49 +0100

    Change response key to "message_id" in createReactionRole

--- src/api/services/reactionRoleService.ts
+++ src/api/services/reactionRoleService.ts
@@ -60,5 +60,5 @@ export async function createReactionRole(request: Request, response: Response, c
         await sentMessage.react(emojiRole.emoji);
     }
 
-    response.status(201).json({ messageId: sentMessage.id }).send();
+    response.status(201).json({ message_id: sentMessage.id }).send();
 }
