c6225759b60da7e2a48e54b2c7eefcd4179a6fb2
silentesc <scope-frisk-copy@duck.com>
2025-03-27 17:23:56 +0100

    Enhance error handling in reaction role fetching and improve code readability

--- src/main.ts
+++ src/main.ts
@@ -51,9 +51,21 @@ client.once("ready", async () => {
         return;
     }
 
-    const guildIds: Array<string> = client.guilds.cache.map(guild => guild.id);
+    const guildIds: Array<string> = client.guilds.cache.map((guild) => guild.id);
     for (const guildId of guildIds) {
-        const response = await axios.get(`${process.env.BACKEND_URL}/reaction_role/reaction_roles/${guildId}`, { params: { "api_key": process.env.API_KEY } });
+        const response = await axios
+            .get(
+                `${process.env.BACKEND_URL}/reaction_role/reaction_roles/${guildId}`,
+                { params: { api_key: process.env.API_KEY } }
+            )
+            .catch((error) => {
+                if (!error.response || error.response.status !== 404) {
+                    throw error;
+                }
+            });
+        if (!response || !response.data) {
+            continue;
+        }
         for (const entry of response.data) {
             reactionRoles.push(entry);
         }
