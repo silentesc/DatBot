833bbb991e381397cd9efa551743ea5edd495089
silentesc <scope-frisk-copy@duck.com>
2025-03-27 18:04:29 +0100

    Add reaction role type handling and validation in createReactionRole endpoint

--- src/utils/cache.ts
+++ src/utils/cache.ts
@@ -3,6 +3,11 @@ import { EmojiRole } from "../api/models";
 
 export let reactionRoles: Array<{ [key: string]: string | Array<EmojiRole> }> = [];
 
+export enum ReactionRoleType {
+    STANDARD = "STANDARD",
+    UNIQUE = "UNIQUE"
+}
+
 export function setReactionRoles(newReactionRoles: Array<{ [key: string]: string | Array<EmojiRole> }>) {
     reactionRoles = newReactionRoles;
 }
