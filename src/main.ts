434dc9b5e76249d5db5cf891b6c55c2a78271ed7
silentesc <scope-frisk-copy@duck.com>
2025-04-05 20:39:30 +0200

    Add logging for fetching reaction roles and improve data retrieval process

--- src/main.ts
+++ src/main.ts
@@ -51,11 +51,22 @@ client.once("ready", async () => {
         return;
     }
 
+    // Load reaction roles and fetch guild, channel, message and reactions users into cache
     const guildIds: Array<string> = client.guilds.cache.map((guild) => guild.id);
     for (const guildId of guildIds) {
         const response = await axios.get(`${process.env.BACKEND_URL}/reaction_role/reaction_roles/${guildId}`, { params: { api_key: process.env.API_KEY } });
         for (const entry of response.data) {
+            console.log("Fetching data for reaction roles from guild", guildId);
+            const channelId = entry["channel_id"]
+            const messageId = entry["message_id"]
+            const guild = await client.guilds.fetch(guildId);
+            const channel = await guild.channels.fetch(channelId);
+            if (channel && channel.isTextBased()) {
+                const message = await channel.messages.fetch(messageId);
+                message.reactions.cache.map(r => r.users.fetch());
+            }
             reactionRoles.push(entry);
+            console.log("Fetch complete for guild", guildId);
         }
     }
 
