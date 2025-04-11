e9aa577cadfbd6d82c523fbcd5a0bc12a62dd1f1
silentesc <scope-frisk-copy@duck.com>
2025-04-11 18:14:20 +0200

    Added getGuilds endpoint back

--- src/api/services/guildService.ts
+++ src/api/services/guildService.ts
@@ -2,6 +2,23 @@ import { Client } from "discord.js";
 import { Request, Response } from "express";
 
 
+export async function getGuilds(request: Request, response: Response, client: Client) {
+    const authHeader = request.headers.authorization;
+
+    if (!authHeader || authHeader !== process.env.API_KEY) {
+        return response.status(403).json({ error: "Forbidden" }).send();
+    }
+
+    const guilds = client.guilds.cache.map(guild => ({
+        id: guild.id,
+        name: guild.name,
+        icon: guild.icon,
+    }));
+
+    response.status(200).json(guilds).send();
+}
+
+
 export async function getChannels(request: Request, response: Response, client: Client) {
     const authHeader = request.headers.authorization;
 
@@ -10,7 +27,7 @@ export async function getChannels(request: Request, response: Response, client: 
     }
 
     const guildId = request.params.guildId;
-    const guild = (!client.guilds.cache.get(guildId)) ? await client.guilds.fetch(guildId).catch(_ => {}) : client.guilds.cache.get(guildId);
+    const guild = (!client.guilds.cache.get(guildId)) ? await client.guilds.fetch(guildId).catch(_ => { }) : client.guilds.cache.get(guildId);
 
     if (!guild) {
         return response.status(404).json({ error: "Guild not found" }).send();
@@ -46,7 +63,7 @@ export async function getRoles(request: Request, response: Response, client: Cli
     }
 
     const guildId = request.params.guildId;
-    const guild = (!client.guilds.cache.get(guildId)) ? await client.guilds.fetch(guildId).catch(_ => {}) : client.guilds.cache.get(guildId);
+    const guild = (!client.guilds.cache.get(guildId)) ? await client.guilds.fetch(guildId).catch(_ => { }) : client.guilds.cache.get(guildId);
 
     if (!guild) {
         return response.status(404).json({ error: "Guild not found" }).send();
